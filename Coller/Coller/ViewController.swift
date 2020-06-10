7faf55184ae7ccb96f104b0d11e0d5148d713144
Owner <owner@MA3001.local>
2020-06-10 16:35:23 +0900

    added Coller

---
+++ Coller/Coller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coller
+//
+//  Created by Owner on 2020/06/10.
+//  Copyright © 2020 Owner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
